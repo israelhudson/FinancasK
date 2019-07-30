8f44045765ab114d87caf1741dbe4eeb58d66c7d
israelhudson <israelhudson@gmail.com>
2019-07-29 23:14:23 -0300

    Exibindo uma lista simples no listview

--- app/src/main/java/xyz/ihudapp/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/xyz/ihudapp/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,6 +2,8 @@ package xyz.ihudapp.financask.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import xyz.ihudapp.financask.R
 
 class ListaTransacoesActivity : AppCompatActivity() {
@@ -10,6 +12,15 @@ class ListaTransacoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$ 20,50",
+            "Economia - R$ 100,00")
+
+        val arrayAdapter = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1,
+            transacoes)
+
+        lista_transacoes_listview.setAdapter(arrayAdapter)
     }
 
 }
