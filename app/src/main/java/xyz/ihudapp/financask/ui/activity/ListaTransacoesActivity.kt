821f346a2588b701cf1383b42d7286ea1ebab7b6
israelhudson <israelhudson@gmail.com>
2019-08-05 23:05:15 -0300

    Adicionando cores nas transações

--- app/src/main/java/xyz/ihudapp/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/xyz/ihudapp/financask/ui/activity/ListaTransacoesActivity.kt
@@ -17,12 +17,25 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf(Transacao(
-            tipo = Tipo.DESPESA, data = Calendar.getInstance(), valor = BigDecimal(20.5)),
-            Transacao(valor = BigDecimal(100.0), tipo = Tipo.RECEITA,
-                categoria = "Economia"))
+        val transacoes = listOf(
+            Transacao(
+                tipo = Tipo.DESPESA, data = Calendar.getInstance(), valor = BigDecimal(20.5)
+            ),
+            Transacao(
+                valor = BigDecimal(100.0), tipo = Tipo.RECEITA,
+                categoria = "Economia"
+            ),
+            Transacao(
+                valor = BigDecimal(200.0), tipo = Tipo.DESPESA
+            ),
+            Transacao(
+                valor = BigDecimal(500.0),
+                categoria = "Prêmio",
+                tipo = Tipo.RECEITA
+            )
+        )
 
-        lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(transacoes, this))
+        lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
     }
 
 }
--- app/src/main/java/xyz/ihudapp/financask/ui/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/xyz/ihudapp/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -1,20 +1,24 @@
 package xyz.ihudapp.financask.ui.adapter
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import kotlinx.android.synthetic.main.transacao_item.view.*
 import xyz.ihudapp.financask.R
 import xyz.ihudapp.financask.extension.formataParaBrasileiro
+import xyz.ihudapp.financask.model.Tipo
 import xyz.ihudapp.financask.model.Transacao
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ListaTransacoesAdapter(
     transacoes: List<Transacao>,
-    context: Context) : BaseAdapter(){
+    context: Context
+) : BaseAdapter() {
 
     private val transacoes = transacoes;
     private val context = context;
@@ -26,6 +30,19 @@ class ListaTransacoesAdapter(
         )
 
         val transacao = transacoes[posicao]
+
+        if (transacao.tipo == Tipo.RECEITA) {
+            viewCriada.transacao_valor.
+                setTextColor(ContextCompat.getColor(context, R.color.receita))
+        } else {
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(context, R.color.despesa))
+        }
+
+        if (transacao.tipo == Tipo.RECEITA) {
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_receita)
+        } else {
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_despesa)
+        }
 
         viewCriada.transacao_valor.text = transacao.valor.toString()
         viewCriada.transacao_categoria.text = transacao.categoria
