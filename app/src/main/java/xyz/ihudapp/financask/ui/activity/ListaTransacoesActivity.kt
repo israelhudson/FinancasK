9d2ea16fbff6bb569e85ecc4eac731479da505d6
israelhudson <israelhudson@gmail.com>
2019-08-21 20:51:18 -0300

    Implementando o DatePickerDialog

--- app/src/main/java/xyz/ihudapp/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/xyz/ihudapp/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,11 +1,18 @@
 package xyz.ihudapp.financask.ui.activity
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import xyz.ihudapp.financask.R
+import xyz.ihudapp.financask.extension.formataParaBrasileiro
 import xyz.ihudapp.financask.model.Tipo
 import xyz.ihudapp.financask.model.Transacao
 import xyz.ihudapp.financask.ui.ResumoView
@@ -28,8 +35,33 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         lista_transacoes_adiciona_receita
             .setOnClickListener {
-                Toast.makeText(this,
-                    "clique de receita", Toast.LENGTH_LONG).show()
+                val view: View = window.decorView
+                val viewCriada = LayoutInflater.from(this).inflate(
+                    R.layout.form_transacao,
+                    view as ViewGroup,
+                    false)
+
+                val ano = 2017
+                val mes = 9
+                val dia = 18
+
+                val hoje = Calendar.getInstance()
+                viewCriada.form_transacao_data.setText(hoje.formataParaBrasileiro())
+                viewCriada.form_transacao_data.setOnClickListener {
+                    DatePickerDialog(this,
+                        DatePickerDialog.OnDateSetListener { view, ano, mes, dia ->
+                            val dataSelecionada = Calendar.getInstance()
+                            dataSelecionada.set(ano, mes, dia)
+                            viewCriada.form_transacao_data
+                                .setText(dataSelecionada.formataParaBrasileiro())
+                        }, ano, mes, dia)
+                        .show()
+                }
+
+                AlertDialog.Builder(this)
+                    .setTitle("Adiciona Receita")
+                    .setView(viewCriada)
+                    .show()
             }
     }
 
